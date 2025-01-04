522bc03178af641782d4052132b19b5af6224880
Labib BENALI <labib9774@gmail.com>
2025-01-04 19:26:09 +0100

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,18 @@ function App() {
         base: `"nav" "main"`, //mobile devices , we have just nav and main
         lg: `"nav nav" "aside main"`, // large devices (wide than 1024px)  like laptop
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
         {/*so grid item on ASIDE will showed only in large  screen above="lg" */}
-        <GridItem area="aside">
-          <GenreList/>
+        <GridItem area="aside" paddingX={4}>
+          <GenreList />
         </GridItem>
       </Show>
       <GridItem area="main">
