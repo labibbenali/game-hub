7250ed5df05d66cbbb8a525573e04401074bf4ed
Labib BENALI <labib9774@gmail.com>
2025-01-05 13:09:34 +0100

    filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,18 +1,21 @@
 import useData from "./useData";
 import { Genre } from "./useGenres";
+import { Platform } from "./usePlateforms";
 
-export interface Platfrom{
-  id:number;
-  name:string;
-  slug:string;
-}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
-  platforms:{platform:Platfrom}[];
+  platforms:{platform:Platform}[];
   metacritic:number;
 }
 
-const useGames=(selectedGenre: Genre | null)=>useData<Game>('/games',{params:{genres: selectedGenre?.id}}, [selectedGenre?.id])
+const useGames=(selectedGenre: Genre | null, selectedPlatform: Platform | null) =>
+useData<Game>('/games',{
+  params:{
+    genres: selectedGenre?.id ,
+    platforms:selectedPlatform?.id
+  }
+}, [selectedGenre?.id, selectedPlatform?.id])
 export default useGames;
