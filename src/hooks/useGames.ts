4c316435786fcb1abefefe91c4171fb0537902b5
Labib BENALI <labib9774@gmail.com>
2025-01-26 18:45:21 +0100

    Refactor- simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,35 +2,36 @@ import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlateforms";
 import APIClient, { FetchResponse } from "../services/api-client";
+import ms from "ms";
 
-const apiClient = new APIClient<Game>('/games');
+const apiClient = new APIClient<Game>("/games");
 
 export interface Game {
   id: number;
   name: string;
-  background_image: string
-  platforms:{platform:Platform}[];
-  metacritic:number;
-  rating_top:number;
+  background_image: string;
+  platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
 }
 
-const useGames=(gameQuery:GameQuery) =>
-useInfiniteQuery<FetchResponse<Game>, Error>({
-  queryKey:['games', gameQuery],
-  queryFn:({pageParam=1})=>
-   apiClient.getAll({
-    params:{
-      genres: gameQuery.genreId,
-      parent_platforms: gameQuery.platformId,
-      ordering: gameQuery.sortOrder,
-      search:gameQuery.searchText,
-      page: pageParam
+const useGames = (gameQuery: GameQuery) =>
+  useInfiniteQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: ({ pageParam = 1 }) =>
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genreId,
+          parent_platforms: gameQuery.platformId,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+          page: pageParam,
+        },
+      }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
     },
-  }),
-  getNextPageParam:(lastPage, allPages)=>{
-    return lastPage.next ? allPages.length + 1 : undefined;
-  },
-  staleTime: 24 * 60 * 60 * 1000 //24h
-});
+    staleTime: ms("24h"),
+  });
 
 export default useGames;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/api-client";
+import ms from 'ms';
 import genres from "../data/genres";
+import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Genre>("/genres");
 export interface Genre {
@@ -12,6 +13,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
+    staleTime: ms('24h'),
     initialData: genres,
   });
 export default useGenres;
