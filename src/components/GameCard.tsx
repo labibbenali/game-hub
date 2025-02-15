8675f9afc2225d1aba0437863b24da5781427f0e
Labib BENALI <labib9774@gmail.com>
2025-02-15 21:40:22 +0100

    Refactor- move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import { PlateformList } from "./PlateformList";
 import { CriticScore } from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
@@ -20,7 +20,7 @@ export const GameCard = ({ game }: Props) => {
         </HStack>
         <Heading fontSize="2xl">
           <Link to={"/games/" + game.slug}> {game.name}</Link>
-           <Emoji rating={game.rating_top} />
+          <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
     </Card>
--- src/components/PlateformList.tsx
+++ src/components/PlateformList.tsx
@@ -12,7 +12,7 @@ import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
 
 import { IconType } from "react-icons/lib";
-import { Platform } from "../hooks/usePlateforms";
+import { Platform } from "../entities/Platform";
 
 interface Props {
   platforms: Platform[];
