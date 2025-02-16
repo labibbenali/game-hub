5c7702662923c257b103d4fec28f4a64e355e4f6
Labib BENALI <labib9774@gmail.com>
2025-02-16 18:35:38 +0100

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,14 @@
-import { HStack, Image, Spacer, Text, useStatStyles } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import { ColorModeSwitch } from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   const navigate = useNavigate();
   const handleImageClick = () => {
-    navigate("/"); // Redirige vers la route souhaitée
+    navigate("/"); 
   };
 
   return (
