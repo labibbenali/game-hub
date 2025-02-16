998ac63df5906bab86bb8863f6186b7297de7b16
Labib BENALI <labib9774@gmail.com>
2025-02-16 17:40:14 +0100

    Build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,9 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import { CriticScore } from "../components/CriticScore";
+import { DefinitionItem } from "../components/DefinitionItem";
 import { ExpandableText } from "../components/ExpandableText";
+import { GameAttributes } from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 
 export const GameDetailPage = () => {
@@ -8,12 +11,12 @@ export const GameDetailPage = () => {
   const { data: game, isLoading, error } = useGame(slug!); // ! : mean that we tell to typescript this element will never be null
 
   if (isLoading) return <Spinner />;
-  console.log("ERROR ", error);
   if (error || !game) throw error;
   return (
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
