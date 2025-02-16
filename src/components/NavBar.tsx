0217f280de1674f36dbab29dcf6c4c4178b0ed17
Labib BENALI <labib9774@gmail.com>
2025-02-16 17:40:02 +0100

    Make image icon redirect to home

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,24 @@ import logo from "../assets/logo.webp";
 import { ColorModeSwitch } from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import { useState } from "react";
-
-
+import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const handleImageClick = () => {
+    navigate("/"); // Redirige vers la route souhaitée
+  };
+
   return (
     <HStack padding="10px">
-      <Image src={logo} padding="4px" borderRadius="10px" boxSize="50px" />
+      <Image
+        src={logo}
+        onClick={handleImageClick}
+        padding="4px"
+        borderRadius="10px"
+        boxSize="50px"
+        cursor="pointer"
+      />
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
